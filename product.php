77e6cfd07f921451379169c0b30015a6e17d6c20
isaac <domain.itcyborg@outlook.com>
2021-10-05 23:02:43 +0300

    Product page

---
+++ product.php
@@ -0,0 +1,189 @@
+<?php
+@include('header.php');
+?>
+<main class="main">
+    <div class="page-content mt-5">
+        <div class="container">
+            <h3 class="mb-5">Wilo-Sub TWI</h3>
+            <div class="col-md-12 bg-light p-2">
+                <ul class="nav nav-tabs" id="tabs-2" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link" id="tab-5-tab" data-toggle="tab" href="#tab-5" role="tab" aria-controls="tab-5" aria-selected="false">Tab 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="tab-6-tab" data-toggle="tab" href="#tab-6" role="tab" aria-controls="tab-6" aria-selected="false">Tab 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="tab-7-tab" data-toggle="tab" href="#tab-7" role="tab" aria-controls="tab-7" aria-selected="true">Tab 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="tab-8-tab" data-toggle="tab" href="#tab-8" role="tab" aria-controls="tab-8" aria-selected="false">Tab 4</a>
+                    </li>
+                </ul>
+                <div class="tab-content tab-content-border" id="tab-content-2">
+                    <div class="tab-pane fade" id="tab-5" role="tabpanel" aria-labelledby="tab-5-tab">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. </p>
+                    </div><!-- .End .tab-pane -->
+                    <div class="tab-pane fade" id="tab-6" role="tabpanel" aria-labelledby="tab-6-tab">
+                        <p>Nobis perspiciatis natus cum, sint dolore earum rerum tempora aspernatur numquam velit tempore omnis, delectus repellat facere voluptatibus nemo non fugiat consequatur repellendus! Enim, commodi, veniam ipsa voluptates quis amet.</p>
+                    </div><!-- .End .tab-pane -->
+                    <div class="tab-pane fade active show" id="tab-7" role="tabpanel" aria-labelledby="tab-7-tab">
+                        <p>Perspiciatis quis nobis, adipisci quae aspernatur, nulla suscipit eum. Dolorum, earum. Consectetur pariatur repellat distinctio atque alias excepturi aspernatur nisi accusamus sed molestias ipsa numquam eius, iusto, aliquid, quis aut.</p>
+                    </div><!-- .End .tab-pane -->
+                    <div class="tab-pane fade" id="tab-8" role="tabpanel" aria-labelledby="tab-8-tab">
+                        <p>Quis nobis, adipisci quae aspernatur, nulla suscipit eum. Dolorum, earum. Consectetur pariatur repellat distinctio atque alias excepturi aspernatur nisi accusamus sed molestias ipsa numquam eius, iusto, aliquid, quis aut.</p>
+                    </div><!-- .End .tab-pane -->
+                </div><!-- End .tab-content -->
+            </div>
+
+            <h3 class="mt-5">MODELS</h3>
+            <hr>
+            <div class="col-md-12">
+                <a class="btn w-100 bg-image mb-2" style="text-align: left !important;justify-content:left;">Some btn</a><br>
+                <a class="btn w-100 bg-image mb-2" style="text-align: left !important;justify-content:left;">Some btn</a><br>
+                <a class="btn w-100 bg-image mb-2" style="text-align: left !important;justify-content:left;">Some btn</a><br>
+                <a class="btn w-100 bg-image mb-2" style="text-align: left !important;justify-content:left;">Some btn</a><br>
+                <a class="btn w-100 bg-image mb-2" style="text-align: left !important;justify-content:left;">Some btn</a><br>
+            </div>
+
+            <h4 class="mt-3">Similar Products</h4>
+            <hr>
+
+            <div class="products text-center">
+
+                <div class="row text-center">
+                    <div class="col-6 col-xl-4 col-xxl-4">
+                        <div class="product">
+                            <figure class="product-media">
+                                <span class="product-label label-new">New</span>
+                                <a href="product.html">
+                                    <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                </a>
+
+                                <div class="product-action-vertical">
+                                    <a href="#" class="btn-product-icon btn-wishlist btn-expandable"><span>add to wishlist</span></a>
+                                </div><!-- End .product-action -->
+
+                                <div class="product-action action-icon-top">
+                                    <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
+                                    <a href="popup/quickView.html" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                    <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                </div><!-- End .product-action -->
+                            </figure><!-- End .product-media -->
+
+                            <div class="product-body">
+                                <div class="product-cat">
+                                    <a href="#">Women</a>
+                                </div><!-- End .product-cat -->
+                                <h3 class="product-title"><a href="product.html">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                <div class="product-price">
+                                    $50.00
+                                </div><!-- End .product-price -->
+                                <div class="ratings-container">
+                                    <div class="ratings">
+                                        <div class="ratings-val" style="width: 0%;"></div><!-- End .ratings-val -->
+                                    </div><!-- End .ratings -->
+                                    <span class="ratings-text">( 0 Reviews )</span>
+                                </div><!-- End .rating-container -->
+
+                                <div class="product-nav product-nav-dots">
+                                    <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                    <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                </div><!-- End .product-nav -->
+                            </div><!-- End .product-body -->
+                        </div><!-- End .product -->
+                    </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+
+                    <div class="col-6 col-xl-4 col-xxl-4">
+                        <div class="product">
+                            <figure class="product-media">
+                                <span class="product-label label-new">New</span>
+                                <a href="product.html">
+                                    <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                </a>
+
+                                <div class="product-action-vertical">
+                                    <a href="#" class="btn-product-icon btn-wishlist btn-expandable"><span>add to wishlist</span></a>
+                                </div><!-- End .product-action -->
+
+                                <div class="product-action action-icon-top">
+                                    <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
+                                    <a href="popup/quickView.html" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                    <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                </div><!-- End .product-action -->
+                            </figure><!-- End .product-media -->
+
+                            <div class="product-body">
+                                <div class="product-cat">
+                                    <a href="#">Women</a>
+                                </div><!-- End .product-cat -->
+                                <h3 class="product-title"><a href="product.html">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                <div class="product-price">
+                                    $50.00
+                                </div><!-- End .product-price -->
+                                <div class="ratings-container">
+                                    <div class="ratings">
+                                        <div class="ratings-val" style="width: 0%;"></div><!-- End .ratings-val -->
+                                    </div><!-- End .ratings -->
+                                    <span class="ratings-text">( 0 Reviews )</span>
+                                </div><!-- End .rating-container -->
+
+                                <div class="product-nav product-nav-dots">
+                                    <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                    <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                </div><!-- End .product-nav -->
+                            </div><!-- End .product-body -->
+                        </div><!-- End .product -->
+                    </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+
+                    <div class="col-6 col-xl-4 col-xxl-4">
+                        <div class="product">
+                            <figure class="product-media">
+                                <span class="product-label label-new">New</span>
+                                <a href="product.html">
+                                    <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                </a>
+
+                                <div class="product-action-vertical">
+                                    <a href="#" class="btn-product-icon btn-wishlist btn-expandable"><span>add to wishlist</span></a>
+                                </div><!-- End .product-action -->
+
+                                <div class="product-action action-icon-top">
+                                    <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
+                                    <a href="popup/quickView.html" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                    <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                </div><!-- End .product-action -->
+                            </figure><!-- End .product-media -->
+
+                            <div class="product-body">
+                                <div class="product-cat">
+                                    <a href="#">Women</a>
+                                </div><!-- End .product-cat -->
+                                <h3 class="product-title"><a href="product.html">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                <div class="product-price">
+                                    $50.00
+                                </div><!-- End .product-price -->
+                                <div class="ratings-container">
+                                    <div class="ratings">
+                                        <div class="ratings-val" style="width: 0%;"></div><!-- End .ratings-val -->
+                                    </div><!-- End .ratings -->
+                                    <span class="ratings-text">( 0 Reviews )</span>
+                                </div><!-- End .rating-container -->
+
+                                <div class="product-nav product-nav-dots">
+                                    <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                    <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                </div><!-- End .product-nav -->
+                            </div><!-- End .product-body -->
+                        </div><!-- End .product -->
+                    </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+                </div>
+            </div>
+        </div><!-- End .container-fluid -->
+    </div><!-- End .page-content -->
+
+</main><!-- End .main -->
+
+<?php
+@include('footer.php')
+?>
